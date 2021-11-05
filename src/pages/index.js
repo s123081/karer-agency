9ace776daca5de6feef80c2c1bf86386527bf6e7
Sinan Karer <sinan.karer@student.ap.be>
2021-11-05 16:23:29 +0100

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
+    <main>
       <Layout pageTitle="Welcome to Karer Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Een Gatsby astronaut"
+        src="../images/gatsby-astronaut.png"
+      />
       </Layout>
+    </main>
   )
 }
 
