6f8ee722e05825930909a38ed7091db7925bbad1
Sinan Karer <sinan.karer@student.ap.be>
2021-11-26 16:30:51 +0100

    Artists page: opmaak en styling

--- src/pages/artists/index.js
+++ src/pages/artists/index.js
@@ -1,39 +1,93 @@
-import * as React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/layout'
+import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../../components/layout"
+import Artist from "../../components/artist"
+import {
+  hero,
+  section,
+  subtitle,
+  artists,
+  description,
+} from "../../page.module.css"
 
-const ArtistsPage = ({data: {allWpArtist: {edges}}}) => {
+// Artists Page Component
+
+const ArtistsPage = ({
+  data: {
+    allWpArtist: { edges: artistsInfo },
+    wpPage: { artistsPage },
+  },
+}) => {
+  const image = getImage(artistsPage.headerArtists.picture.localFile)
   return (
     <Layout pageTitle="Artists of Karer Agency">
-      {edges.map((item) => {
-        const artist = item.node.artistMeta;
-        const slug = item.node.slug;
-        return <Link to={`/artists/${slug}`}>
-          <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
-        </Link>
-
-      })}
+      <GatsbyImage
+        className={hero}
+        image={image}
+        alt={artistsPage.headerArtists.picture.altText}
+      />
+      <div className={section}>
+        <h2 className={subtitle}>{artistsPage.headerArtists.title}</h2>
+        <div
+          className={description}
+          dangerouslySetInnerHTML={{
+            __html: artistsPage.headerArtists.description,
+          }}
+        />
+        <div className={artists}>
+          {artistsInfo.map(({ node: artist }) => (
+            <Artist key={artist.id} slug={artist.slug} artist={artist} />
+          ))}
+        </div>
+      </div>
     </Layout>
   )
 }
 
+// Page Query
+
 export const query = graphql`
   query {
-  allWpArtist {
-    edges {
-      node {
-        artistMeta {
-          firstName
-          lastName
-          artistName
+    wpPage(slug: { eq: "artists" }) {
+      artistsPage {
+        headerArtists {
+          description
+          title
+          picture {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+              }
+            }
+          }
         }
-        id
-        slug
+      }
+    }
+    allWpArtist {
+      edges {
+        node {
+          artistMeta {
+            artistName
+            firstName
+            lastName
+            profilePicture {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    placeholder: BLURRED
+                    transformOptions: { grayscale: true }
+                  )
+                }
+              }
+            }
+          }
+          slug
+          id
+        }
       }
     }
   }
-}
-
 `
 
 export default ArtistsPage
